e5dcdd0d96367fbc689b59f30fe4ce2d07f70bdf
AlessioRoman <alessio.romano02dev@gmail.com>
2023-04-07 11:11:57 +0200

    header + intro added

--- JS-Freelancer/src/App.jsx
+++ JS-Freelancer/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Header from "./Components/Header";
 
 function App() {
-
-  return (
-    <div className="App">
-    	<h1 className='text-red-500'>Hi</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header>
+                <Header />
+            </header>
+            <main>
+                <section>
+                    <div className="text-center mt-20">
+                        <p className="text-grey">Ciao!</p>
+                        <h1 className="text-white font-bold text-4xl">
+                            Portfolio
+                        </h1>
+                    </div>
+                    <div className="mt-4 mb-20 flex justify-center">
+                        <img
+                            src="/dev.png"
+                            alt="char"
+                            className="w-1/3 rounded-full"
+                        />
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- JS-Freelancer/tailwind.config.cjs
+++ JS-Freelancer/tailwind.config.cjs
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      purpleBg: "#0b111e",
+      purpleBgAlt: "#151c28",
+      purpleText: "#9584e6",
+      white: "#ffffff",
+      grey: "#cbd5e1",
+    },
     extend: {},
   },
   plugins: [],
